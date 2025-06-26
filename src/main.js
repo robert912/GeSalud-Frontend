525bb19efc192e06d247b6326836aa80a06aa555
Roberto Orellana Tamayo <roberto.orellana.t@usach.cl>
2025-06-26 17:23:06 -0400

    del access control

--- src/main.js
+++ src/main.js
@@ -83,7 +83,6 @@ const vuetify = createVuetify({
 //Configuracion global de axios
 axios.defaults.baseURL = 'http://localhost:8080';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
 //Crear aplicacion VUE
 const app = createApp(App)
